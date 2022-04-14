6a75527859ec571c90c1227a7c78a1afe435d77a
Zachary <zhoffman@moveworks.ai>
2022-04-14 07:49:10 -0700

    Upgrade talon to use scikit-learn==0.24.1

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools.command.install import install
 
 class InstallCommand(install):
     user_options = install.user_options + [
-        ('no-ml', None, "Don't install without Machine Learning modules."),
+        ("no-ml", None, "Don't install without Machine Learning modules."),
     ]
 
-    boolean_options = install.boolean_options + ['no-ml']
+    boolean_options = install.boolean_options + ["no-ml"]
 
     def initialize_options(self):
         install.initialize_options(self)
@@ -18,46 +18,44 @@ class InstallCommand(install):
         install.finalize_options(self)
         if self.no_ml:
             dist = self.distribution
-            dist.packages=find_packages(exclude=[
-                'tests',
-                'tests.*',
-                'talon.signature',
-                'talon.signature.*',
-            ])
-            for not_required in ['numpy', 'scipy', 'scikit-learn==0.23.1']:
+            dist.packages = find_packages(
+                exclude=[
+                    "tests",
+                    "tests.*",
+                    "talon.signature",
+                    "talon.signature.*",
+                ]
+            )
+            for not_required in ["numpy", "scipy", "scikit-learn==0.24.1"]:
                 dist.install_requires.remove(not_required)
 
 
-setup(name='talon',
-      version='1.4.8',
-      description=("Mailgun library "
-                   "to extract message quotations and signatures."),
-      long_description=open("README.rst").read(),
-      author='Mailgun Inc.',
-      author_email='admin@mailgunhq.com',
-      url='https://github.com/mailgun/talon',
-      license='APACHE2',
-      cmdclass={
-          'install': InstallCommand,
-      },
-      packages=find_packages(exclude=['tests', 'tests.*']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          "lxml>=2.3.3",
-          "regex>=1",
-          "numpy",
-          "scipy",
-          "scikit-learn==0.23.1", # pickled versions of classifier, else rebuild
-          'chardet>=1.0.1',
-          'cchardet>=0.3.5',
-          'cssselect',
-          'six>=1.10.0',
-          'html5lib'
-          ],
-      tests_require=[
-          "mock",
-          "nose>=1.2.1",
-          "coverage"
-          ]
-      )
+setup(
+    name="talon",
+    version="1.4.9",
+    description=("Mailgun library " "to extract message quotations and signatures."),
+    long_description=open("README.rst").read(),
+    author="Mailgun Inc.",
+    author_email="admin@mailgunhq.com",
+    url="https://github.com/mailgun/talon",
+    license="APACHE2",
+    cmdclass={
+        "install": InstallCommand,
+    },
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        "lxml>=2.3.3",
+        "regex>=1",
+        "numpy",
+        "scipy",
+        "scikit-learn==0.24.1",  # pickled versions of classifier, else rebuild
+        "chardet>=1.0.1",
+        "cchardet>=0.3.5",
+        "cssselect",
+        "six>=1.10.0",
+        "html5lib",
+    ],
+    tests_require=["mock", "nose>=1.2.1", "coverage"],
+)
